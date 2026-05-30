edc6d388c4bb3f02b4b2c8e050762ee8f77c26c9
anantmulchandani <anantmulchandani@gmail.com>
2026-05-30 14:39:05 +0530

    Coordinates updated and bg video look changed Updated

--- js/scripts.js
+++ js/scripts.js
@@ -255,7 +255,7 @@ function initMap() {
 }
 
 function initBBSRMap() {
-    var la_fiesta = {lat: 20.305826, lng: 85.85480189999998};
+    var la_fiesta = {lat: 28.3863046, lng: 77.3195216};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: la_fiesta,
